3abffe4cd004d5c8f779b6b05bb96676d13e71be
ihrimon <ihrimon147@gmail.com>
2025-05-29 01:03:36 +0600

    CRUD  operation manually

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 /* Uptime Monitoring Application */
 
 const http = require('http');
-const {handleReqRes} = require('./helpers/handleReqRes')
-const environment = require('./helpers/environments')
+const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 // module scafolding
 const app = {};
 
-// configuaration
-app.config = {
-  port: 5000,
-};
+// testing file system
+data.delete('test', 'newFile', (err) => {
+  console.log(err);
+})
 
 // create server
 app.createServer = () => {
